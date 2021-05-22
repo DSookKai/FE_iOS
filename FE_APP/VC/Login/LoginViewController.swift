5bbf691c4f22928f0e995877960804ad83d43391
spqjf12345 <spqjf12345@gmail.com>
2021-05-22 20:49:41 +0900

    add driver mode for mode rendering

--- FE_APP/VC/Login/LoginViewController.swift
+++ FE_APP/VC/Login/LoginViewController.swift
@@ -8,10 +8,13 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
 
     @IBOutlet weak var nameTextfield: UITextField!
     
     @IBOutlet weak var numberTextfield: UITextField!
+    
+    @IBOutlet weak var driverMode: UIButton!
     
     @IBAction func loginButtonClick(_ sender: Any) {
         //authentication
@@ -24,8 +27,15 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-      
+        driverMode.addTarget(self, action: #selector(didTapMode), for: .touchUpInside)
         // Do any additional setup after loading the view.
+    }
+    
+    @objc func didTapMode(){
+        driverMode.isSelected = true
+        driverMode.setTitleColor(UIColor.systemGray, for: .normal)
+       
+  
     }
     
  
