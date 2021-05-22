4e505a6edeba089bbe9a62e723b00b1ff9c60912
spqjf12345 <spqjf12345@gmail.com>
2021-05-22 19:49:01 +0900

    intial

---
+++ FE_APP/VC/Login/LoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LoginViewController.swift
+//  FE_APP
+//
+//  Created by JoSoJeong on 2021/05/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var nameTextfield: UITextField!
+    
+    @IBOutlet weak var numberTextfield: UITextField!
+    
+    @IBAction func loginButtonClick(_ sender: Any) {
+        //authentication
+        
+        
+    }
+    
+    let center = UNUserNotificationCenter.current()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+        // Do any additional setup after loading the view.
+    }
+    
+ 
+    
+
+    
+
+}
