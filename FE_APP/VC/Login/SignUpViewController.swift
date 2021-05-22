0966d5fa4b4540a910f671f1ad4bb9929a6c1dd7
spqjf12345 <spqjf12345@gmail.com>
2021-05-22 20:57:05 +0900

    for merge

--- FE_APP/VC/Login/SignUpViewController.swift
+++ FE_APP/VC/Login/SignUpViewController.swift
@@ -19,7 +19,6 @@ class SignUpViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
