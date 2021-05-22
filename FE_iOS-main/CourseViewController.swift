fa02ef22f34e82463226b3b0646c7b3f49f445e0
KSK9820 <68066104+KSK9820@users.noreply.github.com>
2021-05-23 02:20:40 +0900

    Add files via upload

--- FE_iOS-main/CourseViewController.swift
+++ FE_iOS-main/CourseViewController.swift
@@ -1,9 +1,3 @@
-//
-//  CourseViewController.swift
-//  FE_iOS-main
-//
-//  Created by 김수경 on 2021/05/22.
-//
 import UIKit
 import MapKit
 import CoreLocation
@@ -185,4 +179,3 @@ class CourseViewController: UIViewController, CLLocationManagerDelegate {
  
 
 }
-
